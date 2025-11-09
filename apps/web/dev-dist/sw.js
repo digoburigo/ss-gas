39236155224a6ce1c819ae8e15e5c8164f733743
digoburigo <rodrigobesmeraldino@gmail.com>
2025-11-09 11:05:34 -0300

    chore: add deps

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a702e535'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n7euhi7mv5k"
+    "revision": "0.l26nr61vgoc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
