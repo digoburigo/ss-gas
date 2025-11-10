7e659f25c5eb915ccc75e1103db2a70636bf49cb
digoburigo <rodrigobesmeraldino@gmail.com>
2025-11-10 09:46:19 -0300

    chore: update deps and mobile

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a702e535'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i3uso2g0ttg"
+    "revision": "0.6hk3clgulv4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
