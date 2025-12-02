7582378a2a49cedc3c8c5215590edfabcf883ecd
digoburigo <rodrigobesmeraldino@gmail.com>
2025-12-02 20:25:13 -0300

    build(deps): update deps

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a702e535'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2etj2jjbb6"
+    "revision": "0.pc64pgtu9mo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
