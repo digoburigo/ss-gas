07d14a858fe2fb13790a2a7eb8cf42a1690d5131
digoburigo <rodrigobesmeraldino@gmail.com>
2026-03-20 18:26:46 -0300

    fix: build

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -78,8 +78,11 @@ define(['./workbox-e2640689'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "3ca0b8505b4bec776b69afdba2768812"
+  }, {
     "url": "index.html",
-    "revision": "0.gf4m5ilk7og"
+    "revision": "0.62q3n5iqnec"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
